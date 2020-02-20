7127e6dc4510ea1cd46f178e76706436275b3fe9
jam9516 <joaquin111695@gmail.com>
2020-02-20 13:20:00 -0600

    Se agregan cambios para la subida de archivos

--- src/main/java/com/mimbus/service/util/UploadFileService.java
+++ src/main/java/com/mimbus/service/util/UploadFileService.java
@@ -10,32 +10,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.*;
+
+import com.mimbus.service.UserService;
+import com.mimbus.service.util.ValidationPath;
+
+import liquibase.util.file.FilenameUtils;
 
 @Service
 public class UploadFileService {
 	
-    
-	private static final String upload_folder =  System.getProperty("user.home") + "/uploadingDir/";
+	private final Logger log = LoggerFactory.getLogger(UserService.class);
+	private static final String upload_folder =  System.getProperty("user.home") + "/resources" + File.separator + "nimbus";
+	enum extVideo { MP4, VGA};
+	enum extImage { JPG, PNG};
+	enum extDocs { PDF, CSV};
 	
 	public String saveFile(MultipartFile file) {
-		System.out.print("upload_folder: " + upload_folder);
 			try {
-				//Create Path
-				StringBuilder builder = new StringBuilder();
-				builder.append(System.getProperty("user.home"));
-				builder.append(File.separator);
-				builder.append("images");
-				builder.append(File.separator);
-				builder.append(file.getOriginalFilename());
+					String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+					log.debug("Save File: {}", file);
+					//Create Path
+					List<String> image = new ArrayList();
+					StringBuilder builder = new StringBuilder();
+					ValidationPath.createPath(upload_folder);
+					
+						if (extension.toUpperCase().equals(extVideo.MP4.toString())
+								|| extension.toUpperCase().equals(extVideo.VGA.toString())) {
+							builder.append(upload_folder);
+							builder.append(File.separator);
+							builder.append("video");
+							log.debug("builder Video : {}", builder);
+							ValidationPath.createPath(builder.toString());
+						}
+					
+						if (extension.toUpperCase().equals(extImage.PNG.toString())
+								|| extension.toUpperCase().equals(extImage.JPG.toString())) {
+							builder.append(upload_folder);
+							builder.append(File.separator);
+							builder.append("image");
+							log.debug("builder image : {}", builder);
+							ValidationPath.createPath(builder.toString());
+						}
+					
+						if (extension.toUpperCase().equals(extDocs.PDF.toString())
+								|| extension.toUpperCase().equals(extDocs.CSV.toString())) {
+							builder.append(upload_folder);
+							builder.append(File.separator);
+							builder.append("docs");
+							log.debug("builder docs : {}", builder);
+							ValidationPath.createPath(builder.toString());
+						}
+						
+					builder.append(File.separator);
+					builder.append(file.getOriginalFilename());
+					
+					//Creating and Writing  File
+					byte[] fileBytes = file.getBytes();
+					Path path = Paths.get(builder.toString());
+					Files.write(path, fileBytes);
+					
+					return builder.toString();
 				
-				//Creating and Writing  File
-				byte[] fileBytes = file.getBytes();
-				Path path = Paths.get(builder.toString());
-				Files.write(path, fileBytes);
-				
-				return builder.toString();
-				
-			 }catch(IOException e) {
+			 }catch(IOException e){
 				 
 				return "Failed to store file " + e.getMessage();
 		} 
--- src/main/java/com/mimbus/web/rest/UploadFileResource.java
+++ src/main/java/com/mimbus/web/rest/UploadFileResource.java
@@ -1,12 +1,18 @@
 package com.mimbus.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.mimbus.service.UserService;
 import com.mimbus.service.util.UploadFileService;
 import java.util.logging.*;
 
@@ -17,14 +23,15 @@ import com.mimbus.security.AuthoritiesConstants;
 @RequestMapping("/api")
 public class UploadFileResource {
 	
-
+	private final Logger log = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
     private UploadFileService uploadFileService;
 	
 	@PostMapping("/uploadFile")
 	@PreAuthorize("hasRole(\"" + AuthoritiesConstants.ADMIN + "\")")
 	public String  uploadFile( @RequestParam("file") MultipartFile file) {
-		
+		log.debug("Upload File", file);
 		String path = "";
 		if (!file.isEmpty()) {
 			path = uploadFileService.saveFile(file);
@@ -33,5 +40,5 @@ public class UploadFileResource {
 		}
 	  return path;
 	}
-	 
+	
 }
